fd283a85bda9e4c43d6c156c5f6c77b07255d114
Urt ThirtyPlus <urt30plus@gmail.com>
2019-01-13 00:07:53 -0500

    archive files to gdrive

--- bin/gameslog_cleanup.sh
+++ bin/gameslog_cleanup.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
-URT_HOME=/ssd/urt/UrbanTerror43
-GAMESLOG_ARCHIVE_DIR=${HOME}/backups/game_logs
-
-GAMESLOG_FILE=${URT_HOME}/q3ut4/games.log
-ARCHIVE_FILE=${GAMESLOG_ARCHIVE_DIR}/gameslog_backup_$(date +"%Y-%m-%d").tar.gz
+GAMESLOG_FILE="/ssd/urt/UrbanTerror43/q3ut4/games.log"
+GAMESLOG_ARCHIVE_DIR="${HOME}/gdrive/backups/game_logs"
+GAMESLOG_ARCHIVE_FILE="${GAMESLOG_ARCHIVE_DIR}/games-$(hostname).log"
 
 if [[ ! -e "${GAMESLOG_FILE}" ]]; then
     echo "$(date): games.log file not found: ${GAMESLOG_FILE}"
     exit 1
 fi
 
-echo "$(date): archiving backup to ${ARCHIVE_FILE}"
-tar cvfz "${ARCHIVE_FILE}" "${GAMESLOG_FILE}"
+echo "$(date): archiving ${GAMESLOG_FILE}"
+cp --verbose --force --backup=simple --suffix=".old" "${GAMESLOG_FILE}" "${GAMESLOG_ARCHIVE_FILE}"
 
-echo "$(date): deleting games.log"
-rm -vf "${GAMESLOG_FILE}"
-
-echo "$(date): delete old archived backup files"
-find ${GAMESLOG_ARCHIVE_DIR}/ -mindepth 1 -type f -name '*.tar.gz' -mtime +180 -delete
+if [[ ! -e "/ssd/urt/run/urt43.pid" ]]; then
+    rm --verbose --force "${GAMESLOG_FILE}"
+fi
--- bin/mysql_backup_b3.sh
+++ bin/mysql_backup_b3.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-BACKUP_DIR=/ssd/urt/backups/b3_db
-ARCHIVE_DIR=${HOME}/backups/b3_db
+BACKUP_DIR="/ssd/urt/backups/b3_db"
+ARCHIVE_DIR="${HOME}/backups/b3_db"
+GDRIVE_BACKUP_DIR="${HOME}/gdrive/backups/b3_db"
 
 if [[ -z $B3_DB_USER ]]; then
     echo "B3_DB_USER env var must be defined"
@@ -16,8 +17,8 @@ if [[ -z $B3_DB_NAME ]]; then
 fi
 
 dt=$(date +"%Y-%m-%d")
-BACKUP_FILE=${BACKUP_DIR}/${B3_DB_NAME}_backup_${dt}.dump
-LOG_FILE=${BACKUP_DIR}/${B3_DB_NAME}_backup_${dt}.log
+BACKUP_FILE="${BACKUP_DIR}/${B3_DB_NAME}_backup_${dt}.dump"
+LOG_FILE="${BACKUP_DIR}/${B3_DB_NAME}_backup_${dt}.log"
 
 echo "starting backup of ${B3_DB_NAME} into file ${BACKUP_FILE}" >${LOG_FILE}
 mysqldump -u ${B3_DB_USER} -p${B3_DB_PASS} ${B3_DB_NAME} >${BACKUP_FILE} 2>>${LOG_FILE}
@@ -27,7 +28,11 @@ find ${BACKUP_DIR}/ -mindepth 1 -type f -name '*.dump' -mtime +14 -delete
 find ${BACKUP_DIR}/ -mindepth 1 -type f -name '*.log' -mtime +14 -delete
 
 echo "archiving backup"
-tar cvfz "${ARCHIVE_DIR}/${B3_DB_NAME}_backup_${dt}.tar.gz" ${BACKUP_FILE} ${LOG_FILE}
+ARCHIVE_FILE="${ARCHIVE_DIR}/${B3_DB_NAME}_backup_${dt}.tar.gz"
+tar -czvf "${ARCHIVE_FILE}" -C "${BACKUP_DIR}" $(basename ${BACKUP_FILE}) $(basename ${LOG_FILE})
 
 echo "delete old archived backup files"
 find ${ARCHIVE_DIR}/ -mindepth 1 -type f -name '*.tar.gz' -mtime +180 -delete
+
+echo "archiving backup to Google Drive"
+cp --verbose "${ARCHIVE_FILE}" "${GDRIVE_BACKUP_DIR}/${B3_DB_NAME}_backup-$(hostname).tar.gz"
