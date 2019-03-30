2bbca5c7de8cfb97f4809ec6a1c17c1a0c3d2114
Urt ThirtyPlus <urt30plus@gmail.com>
2019-03-30 12:38:57 -0500

    added a sudoers.d so `urtadmin` user can run the root level commands

--- bin/weekly.sh
+++ bin/weekly.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 echo "$(date): stopping b3 service"
-systemctl stop urt43_b3
+sudo systemctl stop urt43_b3
 
 echo "$(date): stopping game service"
-systemctl stop urt43_game
+sudo systemctl stop urt43_game
 
 echo "$(date): cleaning up games.log"
-su - urtadmin -c /game/bin/gameslog_cleanup.sh
+/game/bin/gameslog_cleanup.sh
 
 echo "$(date): updating OS packages"
-apt-get --yes --quiet update
-apt-get --yes --quiet upgrade
+sudo apt-get --yes --quiet update
+sudo apt-get --yes --quiet upgrade
 
 echo "$(date): rebooting"
-/sbin/reboot
+sudo /sbin/reboot
