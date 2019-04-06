b02795d1bbfe0467ec7e379c3286f4b37d6f08a8
Urt ThirtyPlus <urt30plus@gmail.com>
2019-04-06 13:46:41 -0500

    add `ioq3` source directory for source builds add URT_EXE env var to allow overriding the ioq3 binary to run UrT with

--- README.md
+++ README.md
@@ -1,18 +1,16 @@
 UrT Game Server
 ===============
 
-Required Environment Variables
+Environment Variables
 ------------------------------
 
-URT_HOME
-URT_HOME_MOD
-B3_HOME
+`URT_EXE`
 
 Optional Environment Variables
 ------------------------------
 
 The following are optional if using MySQL as the storage engine for B3:
 
-B3_DB_USER
-B3_DB_PASS
-B3_DB_NAME
+`B3_DB_USER`
+`B3_DB_PASS`
+`B3_DB_NAME`
--- bin/start_urt.sh
+++ bin/start_urt.sh
@@ -3,8 +3,23 @@
 urt_home=/game/UrbanTerror43
 urt_stdout=/game/logs/urt43_run.log
 
+if [[ -z $URT_EXE ]]; then
+    URT_EXE="${urt_home}/Quake3-UrT-Ded.x86_64"
+    echo "$(date): env var URT_EXE not set, defaulting to [${URT_EXE}]" >${urt_stdout}
+else
+    echo "$(date): env var URT_EXE set to [${URT_EXE}]" >${urt_stdout}
+fi
+
 echo "$(date): Starting UrT 4.3" >${urt_stdout}
 
-nohup ${urt_home}/Quake3-UrT-Ded.x86_64 +set fs_game q3ut4 +set fs_basepath ${urt_home} +set fs_homepath ${urt_home} +set dedicated 2 +set net_port 27961 +set com_hunkmegs 128 +set ttycon 0 +exec server.cfg >/dev/null 2>>${urt_stdout} &
+nohup ${URT_EXE} \
+	+set fs_game q3ut4 \
+	+set fs_basepath ${urt_home} \
+	+set fs_homepath ${urt_home} \
+	+set dedicated 2 \
+	+set net_port 27961 \
+	+set com_hunkmegs 128 \
+	+set ttycon 0 \
+	+exec server.cfg >/dev/null 2>>${urt_stdout} &
 
 echo $! >/game/run/urt43.pid
