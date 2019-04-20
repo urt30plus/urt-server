6344cb9aeff18056a65160045d03173fe7080a0a
Urt ThirtyPlus <urt30plus@gmail.com>
2019-04-20 14:32:11 -0500

    source env file for URT_EXE setting fix output piping

--- bin/start_urt.sh
+++ bin/start_urt.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
 urt_home=/game/UrbanTerror43
+urt_env=/game/run/urt.env
 urt_stdout=/game/logs/urt43_run.log
+
+echo "$(date): Starting UrT 4.3" >${urt_stdout}
+
+if [[ -e "${urt_env}" ]]; then
+    source "${urt_env}"
+fi
 
 if [[ -z $URT_EXE ]]; then
     URT_EXE="${urt_home}/Quake3-UrT-Ded.x86_64"
-    echo "$(date): env var URT_EXE not set, defaulting to [${URT_EXE}]" >${urt_stdout}
+    echo "$(date): env var URT_EXE not set defaulting to [${URT_EXE}]" >>${urt_stdout}
 else
-    echo "$(date): env var URT_EXE set to [${URT_EXE}]" >${urt_stdout}
+    echo "$(date): env var URT_EXE set to [${URT_EXE}]" >>${urt_stdout}
 fi
-
-echo "$(date): Starting UrT 4.3" >${urt_stdout}
 
 nohup ${URT_EXE} \
 	+set fs_game q3ut4 \
