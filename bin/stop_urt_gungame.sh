b3a47e29fdd56b69e0c6afe500c3fb9d649febee
Urt ThirtyPlus <urt30plus@gmail.com>
2023-08-07 15:47:02 +0000

    remove unused scripts

--- bin/stop_urt_gungame.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-urt_stdout=/game/logs/urt43_run_gungame.log
-
-echo "$(date): Stopping UrT 4.3" >>${urt_stdout}
-
-kill $(cat /game/run/urt43_gungame.pid) >>${urt_stdout} 2>&1
-rm -v /game/run/urt43_gungame.pid >>${urt_stdout} 2>&1
